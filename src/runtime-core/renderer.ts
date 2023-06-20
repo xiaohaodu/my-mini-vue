462d2ef662ea07ba47c9ba2b7c13adf261f112ec
杜晓浩 <1923911906@qq.com>
2023-06-20 23:47:51 +0800

    ✨ feat: 更新element的props

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,6 +1,6 @@
 import { effect } from "../reactivity/effect";
 import { ShapeFlags } from "../shared/ShapeFlags";
-import { isObject } from "../shared/index";
+import { EMPTY_OBJ, isObject } from "../shared/index";
 import { Fragment } from "./VNode";
 import { createComponentInstance, setupComponent } from "./component";
 import { createAppAPI } from "./createApp";
@@ -45,7 +45,27 @@ export function createRenderer(options) {
         }
     }
     function patchElement(n1, n2, container) {
-
+        const oldProps = n1.props || EMPTY_OBJ;
+        const newProps = n2.props || EMPTY_OBJ;
+        const el = (n2.el = n1.el);
+        patchProps(el, oldProps, newProps);
+    }
+    function patchProps(el, oldProps, newProps) {
+        if (oldProps !== newProps) {
+            for (const key in newProps) {
+                const prevProp = oldProps[key];
+                const nextProp = newProps[key];
+                if (prevProp !== nextProp) {
+                    hostPatchProp(el, key, prevProp, nextProp);
+                }
+            }
+            if (oldProps !== EMPTY_OBJ)
+                for (const key in oldProps) {
+                    if (!(key in newProps)) {
+                        hostPatchProp(el, key, oldProps[key], null);
+                    }
+                }
+        }
     }
     function mountElement(vnode, container, parentComponent) {
         const el = (vnode.el = hostCreateElement(vnode.type));
@@ -60,7 +80,7 @@ export function createRenderer(options) {
         const { props } = vnode;
         for (const key in props) {
             const val = props[key];
-            hostPatchProp(el, key, val);
+            hostPatchProp(el, key, null, val);
         }
         hostInsert(el, container);
     };
