607c0bbb57a977de520faeefb04b871b4e7b92a8
杜晓浩 <1923911906@qq.com>
2023-06-20 16:29:22 +0800

    ✨ feat: 实现provide-inject功能

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -4,14 +4,16 @@ import { initProps } from "./componentProps";
 import { PublicInstanceProxyHandlers } from "./componentPublicInstance";
 import { initSlots } from "./componentSlots";
 
-export function createComponentInstance(vnode) {
+export function createComponentInstance(vnode, parent) {
     const component = {
         vnode,
         type: vnode.type,
         setupState: {},
         el: null,
         slots: null,
-        emit: () => { }
+        provides: parent ? parent.provides : {},
+        parent: {},
+        emit: () => { },
     };
     component.emit = emit.bind(null, component) as any;
     return component;
--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -3,16 +3,16 @@ import { isObject } from "../shared/index";
 import { Fragment } from "./VNode";
 import { createComponentInstance, setupComponent } from "./component";
 
-export function render(vnode, container) {
+export function render(vnode, container, parentComponent) {
     //
-    patch(vnode, container);
+    patch(vnode, container, parentComponent);
 }
-function patch(vnode, container) {
+function patch(vnode, container, parentComponent) {
     const { type, shapeFlag } = vnode;
 
     switch (type) {
         case Fragment:
-            processFragment(vnode, container);
+            processFragment(vnode, container, parentComponent);
             break;
         case Text:
             processText(vnode, container);
@@ -20,23 +20,23 @@ function patch(vnode, container) {
     }
     //处理组件
     if (shapeFlag & ShapeFlags.ELEMENT) {
-        processElement(vnode, container);
+        processElement(vnode, container, parentComponent);
     } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
-        processComponent(vnode, container);
+        processComponent(vnode, container, parentComponent);
     }
 }
-function processFragment(vnode, container) {
-    mountChildren(vnode, container);
+function processFragment(vnode, container, parentComponent) {
+    mountChildren(vnode, container, parentComponent);
 };
-function processElement(vnode, container) {
-    mountElement(vnode, container);
+function processElement(vnode, container, parentComponent) {
+    mountElement(vnode, container, parentComponent);
 }
-function mountElement(vnode, container) {
+function mountElement(vnode, container, parentComponent) {
     const el = (vnode.el = document.createElement(vnode.type));
     if (vnode.shapeFlag & ShapeFlags.TEXT_CHILDREN) {
         el.textContent = vnode.children;
     } else if (vnode.shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
-        mountChildren(vnode, el);
+        mountChildren(vnode, el, parentComponent);
     }
     for (const key in vnode.props) {
         el.setAttribute(key, vnode.props[key]);
@@ -50,24 +50,24 @@ function mountElement(vnode, container) {
     }
     container.append(el);
 };
-function processComponent(vnode, container) {
-    mountComponent(vnode, container);
+function processComponent(vnode, container, parentComponent) {
+    mountComponent(vnode, container, parentComponent);
 }
-function mountComponent(initialvnode, container) {
-    const instance = createComponentInstance(initialvnode);
+function mountComponent(initialvnode, container, parentComponent) {
+    const instance = createComponentInstance(initialvnode, parentComponent);
     setupComponent(instance);
     setupRenderEffect(instance, initialvnode, container);
 }
-function mountChildren(vnode, container) {
+function mountChildren(vnode, container, parentComponent) {
     for (const val of vnode.children) {
-        patch(val, container);
+        patch(val, container, parentComponent);
     }
 }
 function setupRenderEffect(instance, initialvnode, container) {
     const subTree = instance.render.call(instance.proxy);
     //vnode -> patch
     //vnode -> element -> mount
-    patch(subTree, container);
+    patch(subTree, container, instance);
     initialvnode.el = subTree.el;
 }
 
