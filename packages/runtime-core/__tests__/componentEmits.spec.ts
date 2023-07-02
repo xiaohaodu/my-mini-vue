8e4f969d3860d78106db46205f610904b99eede1
杜晓浩 <1923911906@qq.com>
2023-07-02 12:33:47 +0800

    🦄 refactor: monorepo重构

---
+++ packages/runtime-core/__tests__/componentEmits.spec.ts
@@ -0,0 +1,73 @@
+import { nodeOps, render, h } from "@mini-vue/runtime-test";
+import {vi} from 'vitest'
+
+describe("component: emits", () => {
+  test("trigger handlers", () => {
+    const Foo = {
+      render() {
+        return h("foo");
+      },
+      setup(props, { emit }) {
+        // the `emit` function is bound on component instances
+        emit("foo");
+        emit("bar");
+      },
+    };
+
+    const onfoo = vi.fn();
+    const onBar = vi.fn();
+    const Comp = {
+      render() {
+        return h(Foo, { onfoo, onBar });
+      },
+    };
+    render(h(Comp), nodeOps.createElement("div"));
+
+    expect(onfoo).not.toHaveBeenCalled();
+    // only capitalized or special chars are considered event listeners
+    expect(onBar).toHaveBeenCalled();
+  });
+
+  test("trigger camelCase handler", () => {
+    const Foo = {
+      render() {
+        return h("foo");
+      },
+      setup(props, { emit }) {
+        emit("test-event");
+      },
+    };
+
+    const fooSpy = vi.fn();
+    const Comp = {
+      render() {
+        return h(Foo, { onTestEvent: fooSpy });
+      },
+    };
+    render(h(Comp), nodeOps.createElement("div"));
+
+    expect(fooSpy).toHaveBeenCalledTimes(1);
+  });
+
+  test("trigger kebab-case handler", () => {
+    const Foo = {
+      render() {
+        return h("foo");
+      },
+      setup(props, { emit }) {
+        emit("test-event");
+      },
+    };
+
+    const fooSpy = vi.fn();
+
+    const Comp = {
+      render() {
+        return h(Foo, { "onTest-event": fooSpy });
+      },
+    };
+    render(h(Comp), nodeOps.createElement("div"));
+
+    expect(fooSpy).toHaveBeenCalledTimes(1);
+  });
+});
